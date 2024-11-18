698fad53c51536ca9c9b1492a247c9487470e91b
Gorilla Gigabytes <dave.augustus11@gmail.com>
2024-11-19 02:39:00 +0530

    Accept Table
    Signed-off-by: Gorilla Gigabytes <dave.augustus11@gmail.com>

--- microservices/the_monkeys_gateway/internal/user_service/models.go
+++ microservices/the_monkeys_gateway/internal/user_service/models.go
@@ -1,5 +1,7 @@
 package user_service
 
+import "encoding/json"
+
 type ProfileRequestBody struct {
 	Id int64 `json:"id"`
 }
@@ -45,4 +47,58 @@ type CoAuthor struct {
 type Topics struct {
 	Topics   []string `json:"topics"`
 	Category string   `json:"category"`
+}
+
+type TableData struct {
+	WithHeadings bool       `json:"withHeadings"`
+	Stretched    bool       `json:"stretched"`
+	Content      [][]string `json:"content"`
+}
+
+type Block struct {
+	ID     string      `json:"id"`
+	Type   string      `json:"type"`
+	Data   interface{} `json:"data"` // Use interface{} for polymorphism
+	Author []string    `json:"author"`
+	Time   int64       `json:"time"`
+}
+
+type Blog struct {
+	Time   int64   `json:"time"`
+	Blocks []Block `json:"blocks"`
+}
+
+func (b *Block) UnmarshalJSON(data []byte) error {
+	var temp struct {
+		ID     string          `json:"id"`
+		Type   string          `json:"type"`
+		Data   json.RawMessage `json:"data"`
+		Author []string        `json:"author"`
+		Time   int64           `json:"time"`
+	}
+
+	// Unmarshal common fields
+	if err := json.Unmarshal(data, &temp); err != nil {
+		return err
+	}
+
+	// Assign common fields
+	b.ID = temp.ID
+	b.Type = temp.Type
+	b.Author = temp.Author
+	b.Time = temp.Time
+
+	// Handle polymorphic Data field
+	switch temp.Type {
+	case "table":
+		var tableData TableData
+		if err := json.Unmarshal(temp.Data, &tableData); err != nil {
+			return err
+		}
+		b.Data = tableData
+	default:
+		b.Data = temp.Data // Keep as raw JSON for unknown types
+	}
+
+	return nil
 }
