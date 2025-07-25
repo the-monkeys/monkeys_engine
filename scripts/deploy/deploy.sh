c611e1109a0164ca8dd2ac66cc65047f839541b0
Dave Augustus <devpandey19924u@gmail.com>
2025-07-25 15:30:00 +0530

    Blog search with search query

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+set -e
+
 # git pull origin main
 git pull origin main
 
 # remove docker-compose.yaml file and config\config.yaml
-rm docker-compose.yaml config/config.yaml
+rm ~/monkeys-engine/docker-compose.yml ~/monkeys-engineconfig/config.yaml
 
-# copy docker-compose.yaml and config/config.yaml from user's home directory
-cp ~/monkeys_engine/docker-compose.yaml .
-cp ~/monkeys_engine/config/config.yaml config/
+# copy docker-compose.yml and config/config.yml from user's home directory
+cp ~/docker-compose.yml ~/monkeys-engine/docker-compose.yml
+cp ~/config.yaml ~/monkeys-engine/config/
 
 # restart docker containers
-docker-compose down
-docker-compose up -d    
+sudo docker-compose down
+sudo docker-compose up -d
 
 # wait for the containers to be up
 echo "Waiting for containers to start..."
