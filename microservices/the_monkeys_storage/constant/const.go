e26dccfab94b0015ce8932a9a7c57abb14582708
Gorilla Gigabytes <mail.ppande11@gmail.com>
2025-08-29 16:16:04 +0000

    fixed syntax issues
    Signed-off-by: Gorilla Gigabytes <mail.ppande11@gmail.com>

--- microservices/the_monkeys_storage/constant/const.go
+++ microservices/the_monkeys_storage/constant/const.go
@@ -3,5 +3,7 @@ package constant
 const (
 	ProfileDir          = "profile"
 	BlogDir             = "blogs"
+	LocalBlogsDir       = "local_blogs"
+	LocalProfilesDir    = "local_profiles"
 	DefaultProfilePhoto = "https://github.com/the-monkeys/the_monkeys/raw/main/static/default.jpg"
 )
